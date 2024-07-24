221b82abaf5dc61c1a5d691a3ff4cf0b07d7c4aa
Julio Santos <julioc.9730@gmail.com>
2024-07-24 16:48:34 -0300

    Update reports.js

--- cypress/support/reports.js
+++ cypress/support/reports.js
@@ -3,8 +3,6 @@
  */
 
 Cypress.on('test:after:run', (test, runnable) => {
-    cy.screenshot()
-
     const screenshotName = `${runnable.parent.title} - ${test.title}.png`
     const screenshotPath = `screenshots/${Cypress.spec.name}/${screenshotName}`
 
