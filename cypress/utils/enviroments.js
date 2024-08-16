22901bd3298320d568082fce725998042338975a
Julio Santos <julioc.9730@gmail.com>
2024-08-15 21:04:55 -0300

    Update enviroments.js

--- cypress/utils/enviroments.js
+++ cypress/utils/enviroments.js
@@ -3,29 +3,31 @@ function setupEnv(config) {
         throw new Error("The \"config\" object is required");
     }
 
-    const enviroment = process.env.ENV;
+    const environment = process.env.ENV;
 
-    if (!enviroment) {
+    if (!environment) {
         throw new Error("The \"ENV\" environment variable is required. Enter 'stg' or 'dev'");
-    } else {
-        hosts = {
-            stg: process.env.STAGING,
-            dev: process.env.DEV
-        };
     }
 
-    config.baseUrl = hosts[enviroment];
+    const hosts = {
+        stg: process.env.STAGING,
+        dev: process.env.DEV
+    };
+    
+    if (!hosts[environment]) {
+        throw new Error(`The environment value "${environment}" is invalid. Use 'stg' or 'dev'.`);
+    }
+
+    config.baseUrl = hosts[environment];
     return config;
 }
 
 module.exports = {
-    
     /**
      * Retorna a URL base da aplicação.
      * 
      * @param {object} config - Configuração do Cypress.
      * @returns {string} - URL base da aplicação.
      */
-
     setupEnv 
 };
